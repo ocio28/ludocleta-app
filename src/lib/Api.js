015dd7c80e9f8385d79ef90561bfad5736dde637
coloro <colorolabs@gmail.com>
2018-07-06 21:49:38 -0400

    clean

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,51 +1,16 @@
 const stage = '/dev'
 const ApiUrl = 'https://wthwk4ghue.execute-api.us-east-1.amazonaws.com'
-let bearer = null
-
-export function init(token) {
-  if (token !== null) {
-    bearer = 'Bearer ' + token
-  }
-}
-
-export function register(user) {
-  return send('/register', user)
-}
-
-export function sendCode(email) {
-  return send('/code', {email})
-}
-
-export function login(data) {
-  return send('/login', data)
-}
 
 export function games() {
   return send('/games/fetch')
 }
 
-export function getGame(id) {
-  return send('/games/get', {id: id})
-}
-
-export function getUser() {
-  return send('/user/get')
-}
-
-export function devices() {
-  return send('/user/devices/fetch')
-}
-
 function send(path, body) {
-  let headers = {
-    'Content-Type': 'application/json'
-  }
-  if (bearer !== null) {
-    headers['Authorization'] = bearer
-  }
   return fetch(ApiUrl + stage + path, {
     method: 'POST',
-    headers,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: body ? JSON.stringify(body) : {}
   }).then(response).then(success)
 }
