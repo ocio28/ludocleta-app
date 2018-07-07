f24623f8845816bded90e85ec27f5fec4f3e7b16
coloro <colorolabs@gmail.com>
2018-07-06 23:38:21 -0400

    footer

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Ludocleta front app
 
 https://ludocleta.cl
+
+## REST Api
+
+https://github.com/ocio28/ludocleta-api
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,14 +8,22 @@ export default class Footer extends Component {
       <footer>
         <div className="logos">
           <div className="FooterBlock">
+            <a href="https://t.me/lokaljost" target="blank" className="btn btn-link CleanLink">
+              <i className="fab fa-telegram fa-2x"></i>
+              <strong>@lokaljost</strong>
+            </a>
             <a href="https://twitter.com/lokaljost" target="blank" className="btn btn-link CleanLink">
               <i className="fab fa-twitter fa-2x"></i>
               <strong>@lokaljost</strong>
             </a>
+            <a href="https://github.com/ocio28/ludocleta-app" target="blank" className="btn btn-link CleanLink">
+              <i className="fab fa-github fa-2x"></i>
+              <strong> ludocleta-app</strong>
+            </a>
           </div>
           <div className="FooterBlock">
             <img src={logo} alt="Ludocleta" width={40} height={40}/>
-            Ludocleta - 2018
+            <strong>Ludocleta - {new Date().getFullYear()}</strong>
           </div>
         </div>
       </footer>
